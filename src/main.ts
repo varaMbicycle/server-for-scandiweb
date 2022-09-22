9237e1866cfe9e88c376415956cf19f43ce5653d
varachkin <varachkin@gmail.com>
2022-09-22 15:45:04 +0300

    fix: upd ver

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
-import { ApolloServer } from 'apollo-server';
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import express from 'express';
+import  { ApolloServer } from "apollo-server-express";
 
-const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers,
-    cors: {
-        origin: ["https://test-store-scandiweb.herokuapp.com/"]
-    },
+const server = new ApolloServer( {
+    typeDefs,
+    resolvers,
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+const app = express();
+
+server.start().then( () => {
+    server.applyMiddleware({ app });
+    app.get("*", (_: any, res: { redirect: (arg0: string) => void; }) => {
+        res.redirect("/graphql");
+    });
+
+    app.listen( 4000,() =>
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    );
+
+})
+
