ad1f18c0dd53666e58796dc7b6a7d675fd9d1835
varachkin <varachkin@gmail.com>
2022-09-22 15:14:51 +0300

    fix: added express

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server-express"
+import express from 'express'
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
+const app = express()
 const server = new ApolloServer({
     typeDefs: typeDefs,
     resolvers: resolvers,
-    cors: false,
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+server.applyMiddleware({app, cors: false})
+app.listen(80, () => console.log('server stated on port: 80'))
